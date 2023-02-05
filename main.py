b96dc4f55fdfc25501f8d485c8d3e57775244a18
Erick Mancz <48209265+erickmancz@users.noreply.github.com>
2023-02-05 19:15:15 +0000

    converting data structures

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+Author: Erick
+Date: 02/05/2023
+Version: 1.0
+'''
+
+# Meu quinto código
+
+# O Erick tem 29 anos de idade e mora na cidade de São Paulo
+# printar o texto acima com variáveis
+
+nome = 'Erick'
+idade = 29
+idade= str(idade)
+cidade= 'São Paulo'
+
+print('O ' + nome + ' tem ' + idade + ' anos de idade e mora na cidade de ' + cidade + '.')
